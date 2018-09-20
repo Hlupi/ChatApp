6ad356b28dd13b8557e20082256e2bc27caef94b
Iuliia Ashikhmina <yu.ashikhmina@gmail.com>
2018-09-20 16:08:04 +0200

    signUp form

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import SignUp from './screens/SignUp';
+import styles from './App.styles';
+
+export default class ShatApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUp />
+      </View>
+    );
+  }
+}
